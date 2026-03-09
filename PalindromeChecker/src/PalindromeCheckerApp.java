e96858b86f8bcd383302c16a2fe33709d31e8b1c
satyanarr7 <narrakiran3@gmail.com>
2026-03-09 15:48:03 +0530

    UC6:Hardcoded palindrome

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,5 +1,7 @@
-// UC5: Stack-Based Palindrome Checker
+// UC6: Queue + Stack Based Palindrome Check
 
+import java.util.Queue;
+import java.util.LinkedList;
 import java.util.Stack;
 
 public class PalindromeCheckerApp {
@@ -7,25 +9,31 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
         // Original string
-        String text = "radar";
+        String text = "level";
 
-        // Create stack
+        // Create Queue and Stack
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
 
-        // Push characters into stack
+        // Enqueue and Push characters
         for (int i = 0; i < text.length(); i++) {
-            stack.push(text.charAt(i));
+            char ch = text.charAt(i);
+            queue.add(ch);     // Enqueue
+            stack.push(ch);    // Push
         }
 
-        // Pop characters to create reversed string
-        String reversed = "";
+        boolean isPalindrome = true;
 
-        while (!stack.isEmpty()) {
-            reversed = reversed + stack.pop();
+        // Compare dequeue and pop
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
         }
 
-        // Compare original and reversed string
-        if (text.equals(reversed)) {
+        // Display result
+        if (isPalindrome) {
             System.out.println("The string \"" + text + "\" is a Palindrome.");
         } else {
             System.out.println("The string \"" + text + "\" is NOT a Palindrome.");
